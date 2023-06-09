c5196bd5ba3518e8ffc45d4653e53d22896a071f
dobodu <ludovic.boldu@sfr.fr>
2023-06-10 00:09:54 +0200

    Add files via upload
    Initial realease not fully working, I need debug help

---
+++ test_bno08x.py
@@ -0,0 +1,44 @@
+
+from machine import I2C, Pin
+import time
+from bno08x_i2c import BNO08X_I2C
+from bno08x import *
+
+I2C1_SDA = Pin(16)
+I2C1_SCL = Pin(17)
+
+i2c0 = I2C(0, scl=I2C1_SCL, sda=I2C1_SDA, freq=100000, timeout=200000 )
+print(i2c0.scan())
+
+bno = BNO08X_I2C(i2c0, debug=True)
+
+print("Enabling features")
+
+bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+"""bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
+while True:
+    time.sleep(0.5)
+    print("Acceleration:")
+    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+    print("")
+
+    print("Gyro:")
+    gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
+    print("")
+
+    print("Magnetometer:")
+    mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+    print("")
+    print("Rotation Vector Quaternion:")
+    quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
+    print(
+        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
+    )
+    print("")
+"""
