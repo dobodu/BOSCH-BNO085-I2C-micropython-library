3dd2e1f4a55ff28140f7eceee9a199bc95c0a8a2
dobodu <ludovic.boldu@sfr.fr>
2023-06-10 14:14:42 +0200

    Update bno08x_i2c.py
    Simplifying debugging render

--- lib/bno08x_i2c.py
+++ lib/bno08x_i2c.py
@@ -22,8 +22,8 @@ class BNO08X_I2C(BNO08X):
         super().__init__(reset, debug)
 
     def _send_packet(self, channel, data):
-        self._dbg("BNO08X_I2C SENDING PACKET...")
-        self._dbg("BNO08X_I2C SENDING Channel : ", channel, " Data : ", data)
+        #self._dbg("BNO08X_I2C SENDING PACKET...")
+        #self._dbg("BNO08X_I2C SENDING Channel : ", channel, " Data : ", data)
         data_length = len(data)
         write_length = data_length + 4
 
@@ -32,9 +32,11 @@ class BNO08X_I2C(BNO08X):
         self._data_buffer[3] = self._sequence_number[channel]
         for idx, send_byte in enumerate(data):
             self._data_buffer[4 + idx] = send_byte
-        self._dbg("BNO08X_I2C SENDING Data Buffer : ", self._data_buffer[0:write_length])
+        #self._dbg("BNO08X_I2C SENDING Data Buffer : ", self._data_buffer[0:write_length])
         packet = Packet(self._data_buffer)
-        self._dbg("BNO08X_I2C SENDING Packet : ",packet)
+        self._dbg("BNO08X_I2C SENDING Packet : ")
+        if self._debug:
+            print(packet)
         self.bus_device_obj.writeto(self.bno_address, self._data_buffer[0:write_length], True)
 
         self._sequence_number[channel] = (self._sequence_number[channel] + 1) % 256
@@ -45,35 +47,35 @@ class BNO08X_I2C(BNO08X):
 
     def _read_header(self):
         self._dbg("BNO08X_I2C READ HEADER...")
-        """Reads the first 4 bytes available as a header
-        with self.bus_device_obj as i2c:
-            i2c.readinto(self._data_buffer, end=4)  # this is expecting a header
-        packet_header = Packet.header_from_buffer(self._data_buffer)"""
+        self._dbg("")
+        #Reads the first 4 bytes available as a header ==> Expecting a header
         self.bus_device_obj.readfrom_into(self.bno_address, self._data_buffer[0:4])
         packet_header = Packet.header_from_buffer(self._data_buffer)
-        self._dbg(packet_header)
+        self._dbg("\t",packet_header)
+        self._dbg("")
         return packet_header
 
     def _read_packet(self):
         self._dbg("BNO08X_I2C READ PACKET...")
-        #with self.bus_device_obj as i2c:
-        #    i2c.readinto(self._data_buffer, end=4)  # this is expecting a header?
-        # Working better with self._data_buffer but should be self._data_buffer[0:4] ???
+        #Read a packet
+        #Begin with reading a header ==> Expecting a header 
+        #Debugging Working better with self._data_buffer but should be self._data_buffer[0:4] ???
         self.bus_device_obj.readfrom_into(self.bno_address, self._data_buffer)
         self._dbg("")
-        self._dbg("SHTP READ packet header: ", [hex(x) for x in self._data_buffer[0:4]])
+        self._dbg("\tSHTP READ packet header: ", [hex(x) for x in self._data_buffer[0:4]])
 
-        header = Packet.header_from_buffer(self._data_buffer)
+        header = Packet.header_from_buffer(self._data_buffer[0:4])
         packet_byte_count = header.packet_byte_count
         channel_number = header.channel_number
         sequence_number = header.sequence_number
 
         self._sequence_number[channel_number] = sequence_number
         if packet_byte_count == 0:
-            self._dbg("SKIPPING NO PACKETS AVAILABLE IN i2c._read_packet")
+            self._dbg("\tSKIPPING NO PACKETS AVAILABLE IN i2c._read_packet")
+            self._dbg("")
             raise PacketError("No packet available")
         packet_byte_count -= 4
-        self._dbg("Channel", channel_number, "has", packet_byte_count, "bytes available to read")
+        self._dbg("\tChannel", channel_number, "has", packet_byte_count, "bytes available to read")
 
         self._read(packet_byte_count)
 
@@ -87,15 +89,12 @@ class BNO08X_I2C(BNO08X):
 
     # returns true if all requested data was read
     def _read(self, requested_read_length):
-        self._dbg("trying to read", requested_read_length, "bytes")
+        self._dbg("\tTrying to read", requested_read_length, "bytes")
         # +4 for the header
         total_read_length = requested_read_length + 4
         if total_read_length > DATA_BUFFER_SIZE:
             self._data_buffer = bytearray(total_read_length)
-            self._dbg(
-                "!!!!!!!!!!!! ALLOCATION: increased _data_buffer to bytearray(%d) !!!!!!!!!!!!! "
-                % total_read_length
-            )
+            self._dbg("!!!!! Increased _data_buffer to bytearray(%d) !!!!!" % total_read_length)
         self.bus_device_obj.readfrom_into(self.bno_address, self._data_buffer[0:total_read_length])
 
 
@@ -113,5 +112,6 @@ class BNO08X_I2C(BNO08X):
         else:
             ready = header.data_length > 0
 
-        self._dbg("\tdata ready", ready)
+        self._dbg("BNO08X_I2C_DATA READY : ", ready)
+        self._dbg("")
         return ready
