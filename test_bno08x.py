55877c89656c13f1533925cd78efefbf6cd7cba6
dobodu <ludovic.boldu@sfr.fr>
2023-06-10 23:24:39 +0200

    Update test_bno08x.py
    Minor changes

--- test_bno08x.py
+++ test_bno08x.py
@@ -17,13 +17,16 @@ bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_MAGNETOMETER)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)"""
 
+print("**************************************************")
+print("*         ACCELEROMETER   ENABLED                *")
+print("**************************************************")
+
 while True:
     time.sleep(0.5)
     print("Acceleration:")
     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
     print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
     print("")
-
     """print("Gyro:")
     gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
     print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
@@ -38,5 +41,4 @@ while True:
     print(
         "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
     )
-    print("")
-"""
+    print("")"""
