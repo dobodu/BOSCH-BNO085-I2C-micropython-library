eac35646f6c6c527f71274ab65928e80acc680f1
dobodu <ludovic.boldu@sfr.fr>
2023-06-11 21:18:27 +0200

    Update test_bno08x.py
    Final version with all sensors reports and commentaries

--- test_bno08x.py
+++ test_bno08x.py
@@ -1,44 +1,41 @@
+# BNO08X Micropython I2C Test programm by Dobodu
+#
+# This program set up an I2C connection to the BNO08x device
+# Then Create a BNO08X class based object
+# Then enables sensors
+# And finally report sensors every 0.5 seconds.
+#
+# Original Code from Adafruit CircuitPython Library
+
 
 from machine import I2C, Pin
 import time
-from bno08x_i2c import BNO08X_I2C
-from bno08x import *
+from bno08x_i2c import *
 
 I2C1_SDA = Pin(16)
 I2C1_SCL = Pin(17)
 
 i2c0 = I2C(0, scl=I2C1_SCL, sda=I2C1_SDA, freq=100000, timeout=200000 )
-print(i2c0.scan())
+print("I2C Device found at address : ",i2c0.scan(),"\n")
 
-bno = BNO08X_I2C(i2c0, debug=True)
+bno = BNO08X_I2C(i2c0, debug=False)
+print("BNO08x I2C connection : Done\n")
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-"""bno.enable_feature(BNO_REPORT_GYROSCOPE)
 bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)"""
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
-print("**************************************************")
-print("*         ACCELEROMETER   ENABLED                *")
-print("**************************************************")
+print("BNO08x sensors enabling : Done\n")
 
 while True:
     time.sleep(0.5)
-    print("Acceleration:")
     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-    print("")
-    """print("Gyro:")
+    print("Acceleration\tX: %0.6f\tY: %0.6f\tZ: %0.6f\tm/s²" % (accel_x, accel_y, accel_z))
     gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-    print("")
-
-    print("Magnetometer:")
+    print("Gyroscope\tX: %0.6f\tY: %0.6f\tZ: %0.6f\trads/s" % (gyro_x, gyro_y, gyro_z))
     mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-    print("")
-    print("Rotation Vector Quaternion:")
+    print("Magnetometer\tX: %0.6f\tY: %0.6f\tZ: %0.6f\tuT" % (mag_x, mag_y, mag_z))
     quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-    print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-    )
-    print("")"""
+    print("Rot Vect Quat\tI: %0.6f\tJ: %0.6f\tK: %0.6f\tReal: %0.6f" % (quat_i, quat_j, quat_k, quat_real))
+    print("")
