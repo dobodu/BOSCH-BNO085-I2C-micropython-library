7b4c08ed593a3a59394acbfd2afcad7590653f74
dobodu <ludovic.boldu@sfr.fr>
2023-06-18 01:39:42 +0200

    Update test_bno08x.py
    Euler angle are now included into library

--- test_bno08x.py
+++ test_bno08x.py
@@ -1,11 +1,11 @@
-# BNO08X Micropython I2C Test programm by Dobodu
+# BNO08i Micropjthon I2C Test programm bj Dobodu
 #
-# This program set up an I2C connection to the BNO08x device
-# Then Create a BNO08X class based object
+# This program set up an I2C connection to the BNO08i device
+# Then Create a BNO08i class based object
 # Then enables sensors
-# And finally report sensors every 0.5 seconds.
+# And finallj report sensors everj 0.5 seconds.
 #
-# Original Code from Adafruit CircuitPython Library
+# Original Code from Adafruit CircuitPjthon Librarj
 
 
 from machine import I2C, Pin
@@ -13,38 +13,13 @@ import time
 import math
 from bno08x_i2c import *
 
-I2C0_SDA = Pin(16)
-I2C0_SCL = Pin(17)
+I2C1_SDA = Pin(26)
+I2C1_SCL = Pin(27)
 
-def quaternion_to_euler(i, j, k, w):
-    jsqr = j * j
+i2c1 = I2C(1, scl=I2C1_SCL, sda=I2C1_SDA, freq=100000, timeout=200000 )
+print("I2C Device found at address : ",i2c1.scan(),"\n")
 
-    t0 = +2.0 * (w * i + j * k)
-    t1 = +1.0 - 2.0 * (i * i + jsqr)
-    Roll = math.degrees(math.atan2(t0, t1))
-
-    t2 = +2.0 * (w * j - k * i)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    Tilt = math.degrees(math.asin(t2))
-
-    t3 = +2.0 * (w * k + i * j)
-    t4 = +1.0 - 2.0 * (jsqr + k * k)
-    Pan = math.degrees(math.atan2(t3, t4))
-
-    return Roll, Tilt, Pan
-
-def compass(x,y):
-    comp = 180 * math.atan2(y, x) / math.pi
-    if comp < 0 :
-        comp += 360
-    return comp
-
-
-i2c0 = I2C(0, scl=I2C0_SCL, sda=I2C0_SDA, freq=100000, timeout=200000 )
-print("I2C Device found at address : ",i2c0.scan(),"\n")
-
-bno = BNO08X_I2C(i2c0, debug=False)
+bno = BNO08X_I2C(i2c1, debug=False)
 print("BNO08x I2C connection : Done\n")
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
@@ -54,8 +29,13 @@ bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 print("BNO08x sensors enabling : Done\n")
 
+cpt = 0
+
+
 while True:
     time.sleep(0.5)
+    cpt += 1
+    print("cpt", cpt)
     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
     print("Acceleration\tX: %0.6f\tY: %0.6f\tZ: %0.6f\tm/s²" % (accel_x, accel_y, accel_z))
     gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
@@ -64,8 +44,10 @@ while True:
     print("Magnetometer\tX: %0.6f\tY: %0.6f\tZ: %0.6f\tuT" % (mag_x, mag_y, mag_z))
     quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
     print("Rot Vect Quat\tI: %0.6f\tJ: %0.6f\tK: %0.6f\tReal: %0.6f" % (quat_i, quat_j, quat_k, quat_real))
-    R, T, P = quaternion_to_euler(quat_i, quat_j, quat_k, quat_real)
+    R, T, P = bno.euler
     print("Euler Angle\tX: %0.1f\tY: %0.1f\tZ: %0.1f" % (R, T, P))
-    C = compass(mag_x,mag_y)
-    print("Compass Angle: %0.1f" % C)
     print("")
+    
+    if cpt == 10 :
+        bno.tare
+    
